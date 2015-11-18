7a727b4f9fed71822766ebca4bff31da154335c3
Diego Sampaio <chinello@gmail.com>
2015-11-18 15:26:57 -0200

    adding ssh public keys from env vars

--- run.sh
+++ run.sh
@@ -8,4 +8,11 @@ rc-status
 touch /run/openrc/softlevel
 ssh-keygen -A
 
+mkdir -p /root/.ssh/
+
+echo "adding keys:"
+echo $PUBLIC_SSH_KEYS
+
+echo $PUBLIC_SSH_KEYS > /root/.ssh/authorized_keys
+
 exec /usr/sbin/sshd -D
